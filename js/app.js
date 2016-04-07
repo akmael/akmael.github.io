303af4ce7ca3f439f12542f9b28c0a18b226875a
AndraIshmael <ishmaelak@gmail.com>
2016-04-06 20:11:50 -0400

    animations added

--- js/app.js
+++ js/app.js
@@ -1,28 +1,38 @@
-angular.module('portfolioPage', ['ngRoute']);
+angular.module('portfolioPage', ['ngRoute', 'ngAnimate']);
 
 angular.module('portfolioPage')
         .config(function($routeProvider){
            $routeProvider
            .when('/',{
-               templateUrl: '../html/views/home.html'
+               templateUrl: '../html/views/home.html',
+               controller: 'webContorller'
            })
            .when('/video',{
-               templateUrl: "../html/views/video.html"
+               templateUrl: "../html/views/video.html",
+               controller: 'videoController'
            })
            .when('/photos', {
                 templateUrl: "../html/views/photos.html",
                 controller: 'photosController'
            })
-           .when('/skills',{
-                 templateUrl: ""
-           })
-           .when('/experiance', {
-             templateUrl:""
-           })
-           .when('/Education', {
-             templateUrl: ""
-           })
            .otherwise({
              redirectTo: '/'
            });
+        });
+
+
+        // CONTROLLERS ============================================
+        // home page controller
+        angular.module('portfolioPage').controller('webContorller', function($scope) {
+            $scope.pageClass = 'page-home';
+        });
+
+        // about page controller
+        angular.module('portfolioPage').controller('photosController', function($scope) {
+            $scope.pageClass = 'page-about';
+        });
+
+        // contact page controller
+        angular.module('portfolioPage').controller('videoController', function($scope) {
+            $scope.pageClass = 'page-contact';
         });
